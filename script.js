666de7a9411662ca4819971031caa675bcbc39d0
kdevakiran7-arch <kdevakiran7@gmail.com>
2025-11-03 14:35:37 +0530

    Initial commit: Add script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+let result = document.getElementById('result');
+let expression = '';
+
+function appendNumber(number) {
+    expression += number;
+    result.value = expression;
+}
+
+function appendOperator(operator) {
+    expression += operator;
+    result.value = expression;
+}
+
+function clearResult() {
+    expression = '';
+    result.value = '';
+}
+
+function calculateResult() {
+    try {
+        let evalResult = eval(expression);
+        result.value = evalResult;
+        expression = evalResult.toString();
+    } catch (error) {
+        result.value = 'Error';
+        expression = '';
+    }
+}
